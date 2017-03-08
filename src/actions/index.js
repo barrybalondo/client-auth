40730cbbc6f24d52a2bfc4d6e7486c62dc0dfa48
barrybalondo <barry_balondo@hotmail.com>
2017-03-08 01:21:12 -0500

    added plan for action creator

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+
+
+
+export function signinUser({ email, password}) {
+
+  // Submit email/password to the server 
+
+  // if request is good...
+  // - Update state to indicate user is authenticated
+  // - Save the JWT token
+  // - redirect to the route /feature
+
+
+  // if request is bad...
+  // - show an error to the user
+}
